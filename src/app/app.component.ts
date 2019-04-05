1644d1389f635dca446ad2a132e5ee98fbe343e3
Lucas Marques <lucas.msilva09@gmail.com>
2019-04-05 07:33:20 -0300

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Todo } from './models/todo.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,75 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todolistapp';
+
+  public form: FormGroup;
+  public todos: Todo[] = [];
+  title = 'Minhas Tarefas';
+  public mode: string = 'list';
+
+  
+
+  constructor(private fb: FormBuilder){
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
+    this.load();
+  }
+
+
+  
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear(){
+    this.form.reset();
+  }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if(index !== -1){
+      this.todos.splice(index, 1);
+      this.save();
+    }
+  }
+
+  save(){
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+    this.changeMode('list');
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+    this.save();
+  }
+
+  load(){
+    const data = localStorage.getItem('todos');
+    if(data){
+      this.todos = JSON.parse(data);
+    } else{
+      this.todos = [];
+    }
+  }
+
+  changeMode(mode: string){
+    this.mode=mode;
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+    this.save();
+
+  }
 }
